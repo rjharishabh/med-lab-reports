c88c44a98f0f2ea25d87b8f124c8039bc4d55561
Rishabh Ranjan Jha <61203226+rjharishabh@users.noreply.github.com>
2021-12-12 14:04:56 +0530

    Add error pages

---
+++ error/404.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+		<meta charset="UTF-8">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+    	<link rel="stylesheet" href="../css/bootstrap.min.css">
+        <link rel="icon" type="image/ico" href="../imgs/favicon/favicon.ico">
+    	<link rel="apple-touch-icon" sizes="180x180" href="../imgs/favicon/apple-touch-icon.png">
+    	<link rel="icon" type="image/png" sizes="32x32" href="../imgs/favicon/favicon-32x32.png">
+    	<link rel="icon" type="image/png" sizes="16x16" href="../imgs/favicon/favicon-16x16.png">
+    	<link rel="manifest" href="../site.webmanifest">
+    	<title>404 Page Not Found</title>
+    </head>
+    <body>
+        <main class="container">
+			<div class="row m-3">
+				<div class="col-3">
+					<a href="https://med-lab-reports.great-site.net/">
+					<img src="../imgs/icons/logo.png" width="40" height="40" alt="logo"></a>
+				</div>
+				<div class="col-6">
+					<h3 class="text-center font-monospace">Medical Test and Report Management System</h3>
+				</div>
+			</div>
+            <div class="row m-3">
+				<div class="col-md-6 offset-md-3">
+                    <div class="card text-center">
+                      <div class="card-body">
+						  <h1>404 ERROR</h1>
+						  <h2>Page Not Found</h1>
+							  <img src="../imgs/icons/404.jpg" width="450" height="450" alt="404-icon">
+							  <a href="../" class="btn btn-success btn-lg my-6">Return to Homepage</a>
+                      </div>
+                    </div>
+				</div>
+			</div>
+        </main>
+    </body>
+</html>
